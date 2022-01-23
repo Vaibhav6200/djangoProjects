c96be835598de91339c6bc5fa745d049047215ee
vaibhav <vaibhavpaliwal620@gmail.com>
2022-01-23 21:32:38 +0530

    connected templates and navigation links

---
+++ NewProject/carzone/pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from pages import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('home/', views.index, name='home'),
+]
--- Real life Project/carzoneProject/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from pages import views
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
